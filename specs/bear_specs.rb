62da177218a3cda5b94a98880272a4229d2d4da6
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-07 17:21:09 +0100

    take_fish created

--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -8,6 +8,8 @@ class BearTest < MiniTest::Test
   def setup
     @bear = Bear.new("Yoggy", "Grizzly")
     @fish = Fish.new("Nemo")
+    @fish2 = Fish.new("Sammy")
+    @river = River.new("Amazon")
   end
 
 def test_get_bear_name
@@ -16,6 +18,20 @@ end
 
 def test_get_bear_type
   assert_equal("Grizzly", @bear.type)
+end
+
+def test_take_fish_from_the_river__stomach
+@river.add_fish(@fish)
+@river.add_fish(@fish2)
+@bear.take_fish(@river, @fish)
+assert_equal([@fish],@bear.stomach)
+end
+
+def test_take_fish_from_the_river__fishes
+@river.add_fish(@fish)
+@river.add_fish(@fish2)
+@bear.take_fish(@river, @fish)
+assert_equal([@fish2],@river.fishes)
 end
 
 end
